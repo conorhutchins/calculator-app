8444e17eefd211ffec947c88f385fe7f5a61987d
Conor Hutchins <conorhutchins@btinternet.com>
2024-12-06 11:26:45 +0000

    style display

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,8 +5,8 @@ import styles from "../styles/display.module.css";
 const Display: React.FC<DisplayProps> = ({ input, result }) => {
   return (
     <div className={styles.display}>
-      <div>{input || "0"}</div>
-      <div>{result !== "" && `= ${result}`}</div>
+      <div className={styles.input}>{input || "0"}</div>
+      <div className={styles.result}>{result !== "" && `= ${result}`}</div>
     </div>
   );
 };
